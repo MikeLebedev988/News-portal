89cdd9a3f9d66ddb11ed0603ae9c5eec84efde1b
Лебедев Михаил Семенович <lebedev-ms@gse.globse.local>
2023-05-17 18:40:48 +0800

    added tasks from D3.3

--- Wasteland_News/news_portal/models.py
+++ Wasteland_News/news_portal/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import Sum, Max
+from django.db.models import Sum
 
 article = 'AR'
 news = 'NE'
--- Wasteland_News/news_portal/views.py
+++ Wasteland_News/news_portal/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import *
@@ -6,12 +8,27 @@ from .models import *
 # Create your views here.
 class PostsList(ListView):
     model = Post
-    ordering = 'title'
+    ordering = '-date_time'
     template_name = 'posts.html'
     context_object_name = 'posts'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_post'] = None
+        context['length'] = f"Count of news : '{Post.objects.count()}'"
+        return context
+
 
 class PostDetail(DetailView):
-    model=Post
+    model = Post
+    ordering = '-date_time'
     template_name = 'post_detail.html'
-    context_object_name = 'post'
+    context_object_name = 'post_detail'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_post'] = None
+        context['length'] = f"Count of news : '{Post.objects.count()}'"
+        return context
